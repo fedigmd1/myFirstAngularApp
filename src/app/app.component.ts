850172d267dcf883023e9702f32de34cb8a59556
fedi.gmd <gammoudifedi@gmail.com>
2020-12-23 11:58:03 +0100

    add some code to app module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  // `
+  // <h2>
+  // Welcome
+  // </h2>
+
+  // `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'myFirstAngularApp';
+  public title = 'myFirstAngularApp';
+  public greeting = ''
+  public greeting2 = ''
+  public name = 'Fedi Gammoudi'
+  public name2 = ''
+  public message= ""
+
+  onClick(event: any) {
+    console.log('Welcome to Codevolution');
+    this.greeting = event.type
+  }
+  logMessage(value: any) {
+    console.log(value);
+    this.greeting2 = value
+  }
+
 }
+
